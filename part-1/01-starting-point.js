f190ba6c2cf1549a75e538a25e4d4599b4aeca23
Evgeniy Talko <etalko@gmail.com>
2021-05-10 15:51:29 +0300

    feat: renderHtml feature

--- part-1/01-starting-point.js
+++ part-1/01-starting-point.js
@@ -51,8 +51,28 @@ function renderPlainText(data) {
     result += `You earned ${data.totalVolumeCredits} credits\n`;
     return result;
 }
-const res = statement(invoiceObj[0], playsObj);
-console.log(res);
+
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n<ul>\n`;
+
+    for (const perf of data.performances) {
+        result += `<li><b>${perf.play.name}:</b> ${usd(perf.amount / 100)}`;
+        result += `(${perf.audience} seats)</li>\n`;
+    }
+
+    result += `</ul>\n<p>Amount owed is ${usd(data.totalAmount / 100)}</p>\n`;
+    result += `<p>You earned ${data.totalVolumeCredits} credits</p>`;
+    return result;
+}
+
+// const res = statement(invoiceObj[0], playsObj);
+// const htmlRes = htmlStatement(invoiceObj[0], playsObj);
+// console.log(res);
+// console.log(htmlRes);
 
 module.exports.statement = statement;
 module.exports.plays = playsObj;
